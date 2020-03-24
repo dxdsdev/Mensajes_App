2222b36a4fa440b4891b95a94fa1aebf171d40cc
Daniel Romero <croweloper@gmail.com>
2020-03-24 18:50:41 -0500

    Update

--- src/main/java/com/crowlabs/mensajes_app/Mensaje.java
+++ src/main/java/com/crowlabs/mensajes_app/Mensaje.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.crowlabs.mensajes_app;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import lombok.Data;
-import lombok.Synchronized;
 
 /**
  *
@@ -21,7 +13,6 @@ public @Data class Mensaje {
     private String mensaje;
     private String autor_mensaje;
     private String fecha_mensaje;
-    
 
     public Mensaje(int id_mensaje, String mensaje, String autor_mensaje, String fecha_mensaje) {
         this.id_mensaje = id_mensaje;
@@ -32,6 +23,5 @@ public @Data class Mensaje {
 
     public Mensaje() {
     }
-
     
 }
